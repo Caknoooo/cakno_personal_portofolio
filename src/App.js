3316a8ef2cfd5d443cd2864d4d9334c1c6606dd5
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-01-06 06:43:35 +0700

    refactor: imported components and styling image projects

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
+      <Projects />
     </div>
   );
 }
