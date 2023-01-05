a60a2038be39d817d12b995876c9295c91efd7b9
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-01-06 06:46:25 +0700

    feat: added ProjectCard for projects component

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,18 @@
+import { Container, Col, Row } from "react-bootstrap";
+
+export const ProjectCard = ({ title, description, imgUrl }) => {
+  return (
+    <Col sm={6} md={4} >
+      <div className="proj-imgbx image-to-center" >
+        <img
+          src={imgUrl}
+          alt="images"
+        />
+        <div className="proj-txtx">
+          <h4>{title}</h4>
+          <span>{description}</span>
+        </div>
+      </div>
+    </Col>
+  );
+};
