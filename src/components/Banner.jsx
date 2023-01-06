773d2747fb93be6f083a9021895cb2b74dd350e4
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-01-06 22:09:32 +0700

    refactor: modified and styling NavBar and Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,10 @@ import headerImg from "../assets/img/header-img.svg";
 import hero from "../assets/img/hero1.png";
 import hero2 from "../assets/img/hero2.png";
 import hero3 from "../assets/img/hero3.png";
+import TrackVisibility from "react-on-screen";
+import { isVisible } from "@testing-library/user-event/dist/utils";
 import { useState, useEffect } from "react";
+import 'animate.css';
 
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
@@ -17,6 +20,7 @@ export const Banner = () => {
   const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const period = 2000;
+  const [index, setIndex] = useState(1);
 
   useEffect(() => {
     let ticker = setInterval(() => {
@@ -34,53 +38,81 @@ export const Banner = () => {
     let updateText = isDeleting
       ? fullText.substring(0, text.length - 1)
       : fullText.substring(0, text.length + 1);
-    
+
     setText(updateText);
 
     if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
+      setDelta((prevDelta) => prevDelta / 2);
     }
 
     if (!isDeleting && updateText === fullText) {
       setIsDeleting(true);
       setDelta(period);
-    }
-    else if (isDeleting && updateText === '') {
+    } else if (isDeleting && updateText === "") {
       setIsDeleting(false);
       setDelta(500);
+      setIndex(1);
       setLoopNum(loopNum + 1);
     }
-
+    else {
+      setIndex(prevIndex => prevIndex + 1)
+    }
   };
 
   return (
     <section className="banner" id="home">
       <Container>
-        <Row className="align-items-center">
+        <Row className="align-items-center ">
           <Col xs={12} md={6} xl={7}>
-            <span className="tagline">Welcome to My Portofolio</span>
-            <h1>
-              {`Hi, I'm Caknoo`}
-              <span className="wrap"> { text }</span>
-            </h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptates qui fugiat assumenda illum deleniti veniam beatae
-              deserunt magnam, unde, voluptatum corrupti provident.
-              Necessitatibus officiis suscipit magni consectetur et dolorum
-              quaerat! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptates qui fugiat assumenda illum deleniti veniam beatae
-              deserunt magnam, unde, voluptatum corrupti provident.
-              Necessitatibus officiis suscipit magni consectetur et dolorum
-              quaerat!
-            </p>
-            <button onClick={() => console.log("connect")}>
-              Let's Connect <ArrowRightCircle size={25} />
-            </button>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible
+                      ? "animate__animated animate__fadeIn"
+                      : ""
+                  }
+                >
+                  <span className="tagline">Welcome to My Portofolio</span>
+                  <h1>
+                    {`Hi, I'm Caknoo`}
+                    <span className="wrap"> {text}</span>
+                  </h1>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Voluptates qui fugiat assumenda illum deleniti veniam beatae
+                    deserunt magnam, unde, voluptatum corrupti provident.
+                    Necessitatibus officiis suscipit magni consectetur et
+                    dolorum quaerat! Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Voluptates qui fugiat assumenda illum
+                    deleniti veniam beatae deserunt magnam, unde, voluptatum
+                    corrupti provident. Necessitatibus officiis suscipit magni
+                    consectetur et dolorum quaerat! Lorem ipsum dolor sit amet
+                    consectetur adipisicing elit. Mollitia eligendi cum libero
+                    illo nostrum aut debitis, ratione possimus natus, a dolorem
+                    sed quidem ullam minima quibusdam vero officiis facilis
+                    amet.
+                  </p>
+                  <button onClick={() => console.log("connect")}>
+                    Let's Connect <ArrowRightCircle size={25} />
+                  </button>
+                </div>
+              )}
+            </TrackVisibility>
           </Col>
 
           <Col xs={12} md={6} xl={5}>
-            <img src={hero} alt="Header img " />
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__zoomIn" : ""
+                  }
+                >
+                  <img src={hero} alt="Header Img" />
+                </div>
+              )}
+            </TrackVisibility>
           </Col>
         </Row>
       </Container>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,8 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { useState, useEffect } from "react";
+// import { HashLink } from "react-router-hash-link";
+// import { BrowserRouter as Router } from "react-router-dom";
 import Logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
@@ -29,63 +31,78 @@ export const NavBar = () => {
   }, []);
 
   return (
-    <Navbar bg="dark" expand="lg" className={scrolled ? "scrolled" : ""}>
-      <Container>
-        <Navbar.Brand href="#home">
-          <img src={Logo} alt="Logo" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
-          <span className="navbar-toggle-icon"></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link
-              href="#home"
-              className={
-                activeLink === "home" ? "active navbar-link" : "navbar-link"
-              }
-              onClick={() => onUpdateActiveLink("home")}
-            >
-              Home
-            </Nav.Link>
-            <Nav.Link
-              href="#skills"
-              className={
-                activeLink === "skills" ? "active navbar-link" : "navbar-link"
-              }
-              onClick={() => onUpdateActiveLink("skills")}
-            >
-              Skills
-            </Nav.Link>
-            <Nav.Link
-              href="#projects"
-              className={
-                activeLink === "projects" ? "active navbar-link" : "navbar-link"
-              }
-              onClick={() => onUpdateActiveLink("projects")}
-            >
-              Projects
-            </Nav.Link>
-          </Nav>
 
-          <span className="navbar-text">
-            <div className="social-icon">
-              <a href="https://www.linkedin.com/in/m-naufal-badruttamam-526635217/">
-                <img src={navIcon1} alt="" />
-              </a>
-              <a href="https://web.facebook.com/cakno.comel/">
-                <img src={navIcon2} alt="" />
-              </a>
-              <a href="https://www.instagram.com/caknoo_00/">
-                <img src={navIcon3} alt="" />
-              </a>
-            </div>
-            <button className="vvd" onClick={() => console.log("connect")}>
-              <span className="">Let's Connect </span>
-            </button>
-          </span>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navbar bg="dark" expand="lg" className={scrolled ? "scrolled" : ""}>
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={Logo} alt="Logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggle-icon"></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link
+                href="#home"
+                className={
+                  activeLink === "home" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("home")}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href="#skills"
+                className={
+                  activeLink === "skills" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("skills")}
+              >
+                Skills
+              </Nav.Link>
+              <Nav.Link
+                href="#projects"
+                className={
+                  activeLink === "projects"
+                    ? "active navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("projects")}
+              >
+                Projects
+              </Nav.Link>
+              <Nav.Link
+                href="#features"
+                className={
+                  activeLink === "features"
+                    ? "active navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("features")}
+              >
+                Features
+              </Nav.Link>
+            </Nav>
+
+            <span className="navbar-text">
+              <div className="social-icon">
+                <a href="https://www.linkedin.com/in/m-naufal-badruttamam-526635217/">
+                  <img src={navIcon1} alt="" />
+                </a>
+                <a href="https://web.facebook.com/cakno.comel/">
+                  <img src={navIcon2} alt="" />
+                </a>
+                <a href="https://www.instagram.com/caknoo_00/">
+                  <img src={navIcon3} alt="" />
+                </a>
+              </div>
+              <button className="vvd">
+                <span>Let’s Connect</span>
+              </button>
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
   );
 };
