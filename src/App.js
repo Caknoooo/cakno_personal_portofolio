94eeff75b182d32ece475fcdd033ab9189a47f99
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-01-06 22:14:09 +0700

    refactor: styling Projects

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
+import { Footer } from './components/Footer';
+import { Contact } from './components/Contact';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,12 +2,9 @@ import { Container, Col, Row } from "react-bootstrap";
 
 export const ProjectCard = ({ title, description, imgUrl }) => {
   return (
-    <Col sm={6} md={4} >
-      <div className="proj-imgbx image-to-center" >
-        <img
-          src={imgUrl}
-          alt="images"
-        />
+    <Col sm={6} md={4}>
+      <div className="proj-imgbx image-to-center">
+        <img src={imgUrl} alt="images" />
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
