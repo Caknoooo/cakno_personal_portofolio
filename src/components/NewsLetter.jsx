6554e0585f22399898f1c3ef2273086fc89058dd
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-01-07 15:51:37 +0700

    feat: add icon github on footer

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -26,9 +26,7 @@ export const NewsLetter = ({ status, message, onValidated }) => {
       <div className="newsletter-bx wow slideInUp">
         <Row>
           <Col lg={12} md={6} xl={5}>
-            <h3>
-              Subscribe to our Newsletter<br></br> & Never miss latest updates
-            </h3>
+            <h3>If you want to ask personally, <br /> please send your email</h3>
             {status === "sending" && <Alert>Sending...</Alert>}
             {status === "error" && <Alert variant="danger">{message}</Alert>}
             {status === "success" && <Alert variant="success">{message}</Alert>}
