e06826d48ad91ca48c51b1d6692b7497d53b0495
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-01-07 15:46:55 +0700

    refactor: added center banner text styling and added file features to UI

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
-import { Footer } from './components/Footer';
+import { Features } from "./components/Features";
 import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Features />
       <Contact />
       <Footer />
     </div>
